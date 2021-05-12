af79f9f8c4c72da1dca3032f1a475aa3aa843906
Emil <emil.delrosso@elev.ga.ntig.se>
2021-05-12 15:21:35 +0200

    Updaterade back och front end

--- Hemsidan/app.js
+++ Hemsidan/app.js
@@ -1,7 +1,21 @@
+const { json } = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
+const staticDir = __dirname + "\\static\\";
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(staticDir));
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.sendFile(staticDir + "html\\Home.html"));
+app.get('/Catering', (req, res) => res.sendFile(staticDir + "html\\Home.html"));
+
+
+app.post('/Catering', function (req, res) {
+    console.log(req.body.fname);
+    console.log(req.body.message);
+  res.redirect("/")
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
