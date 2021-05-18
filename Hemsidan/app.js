5271b5cd57fc5075670fd3be392982180e1f1908
Emil <emil.delrosso@elev.ga.ntig.se>
2021-05-18 09:12:51 +0200

    Uppdatering
    Har uppdaterat webbserver, man kan nu skicka ett meddelande till servern.
    (HTML sidor nav, fungerar ej.)

--- Hemsidan/app.js
+++ Hemsidan/app.js
@@ -1,5 +1,6 @@
 const { json } = require('express');
-const express = require('express')
+const express = require('express');
+const { get } = require('mongoose');
 const app = express()
 const port = 3000
 const staticDir = __dirname + "\\static\\";
@@ -8,11 +9,13 @@ app.use(express.static(staticDir));
 app.use(express.urlencoded());
 app.use(express.json());
 
+
 app.get('/', (req, res) => res.sendFile(staticDir + "html\\Home.html"));
-app.get('/Catering', (req, res) => res.sendFile(staticDir + "html\\Home.html"));
+
+app.get('/Meny', (req, res) => res.sendFile(staticDir + "html\\Meny.html"));
 
 
-app.post('/Catering', function (req, res) {
+app.post('/Meny', function (req, res) {
     console.log(req.body.fname);
     console.log(req.body.message);
   res.redirect("/")
