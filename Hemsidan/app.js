2a50b77e4835bc9ac79133a1e9039a9b3929253a
Emil <emil.delrosso@elev.ga.ntig.se>
2021-05-26 14:36:15 +0200

    uppdatering

--- Hemsidan/app.js
+++ Hemsidan/app.js
@@ -10,12 +10,23 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 
-app.get('/', (req, res) => res.sendFile(staticDir + "html\\Home.html"));
+app.get('/', (req, res) => {
+  res.sendFile(staticDir + "html\\Home.html")
+});
 
-app.get('/Meny', (req, res) => res.sendFile(staticDir + "html\\Meny.html"));
+app.get('/Meny', (req, res) => {
+  res.sendFile(staticDir + "html\\Meny.html")
+});
 
+app.get('/Catering', (req, res) => {
+  res.sendFile(staticDir + "html\\Catering.html")
+});
 
-app.post('/Meny', function (req, res) {
+app.get('/Kontakta oss', (req, res) => {
+  res.sendFile(staticDir + "html\\Kontakta oss.html")
+});
+
+app.post('/message', function (req, res) {
     console.log(req.body.fname);
     console.log(req.body.message);
   res.redirect("/")
